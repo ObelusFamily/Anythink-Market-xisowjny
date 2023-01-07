8a1a9a7690560c7ecec91da912b2a48d9b65a5bd
CHAUHAN ARUN MOHIT <96196742+arunedocs@users.noreply.github.com>
2023-01-07 12:18:51 +0000

    fixed Image Placeholder

--- frontend/src/components/ItemPreview.js
+++ frontend/src/components/ItemPreview.js
@@ -37,6 +37,7 @@ const ItemPreview = (props) => {
     >
       <img
         alt="item"
+        placeholder={item.image ? null : "placeholder.png"}
         src={item.image ? item.image : "placeholder.png"}
         className="card-img-top item-img"
         style={{ borderRadius: "20px" }}
